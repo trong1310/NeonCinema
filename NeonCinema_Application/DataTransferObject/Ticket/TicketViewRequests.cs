7deb2bcd891f408e3c59d29c904ee41f29a7f7c1
Sir_Giap <140328785+mrgiap736@users.noreply.github.com>
2024-08-01 16:09:30 +0700

    Ticket

---
+++ NeonCinema_Application/DataTransferObject/Ticket/TicketViewRequests.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NeonCinema_Application.DataTransferObject.Ticket
+{
+    public interface TicketViewRequests
+    {
+        public decimal Price { get; set; }
+        public string BarCode { get; set; }
+        public string SeatNumber { get; set; }
+        public DateTime StarTime { get; set; }
+        public DateTime ScreeningDate { get; set; }
+        public string RoomName { get; set; }
+
+
+    }
+}
---
+++ NeonCinema_Application/Interface/ITicketRepository.cs
@@ -0,0 +1,19 @@
+﻿using NeonCinema_Application.DataTransferObject.Ticket;
+using NeonCinema_Domain.Database.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NeonCinema_Application.Interface
+{
+    public interface ITicketRepository
+    {
+        Task<bool> CreateTicket(Ticket ticket, CancellationToken cancellationToken);
+        Task<bool> UpdateTicket(Ticket ticket, CancellationToken cancellationToken);
+        Task<bool> DeleteTicket(Guid id, CancellationToken cancellationToken);
+        Task<List<TicketDTO>> GetAllTicket(CancellationToken cancellationToken);
+        Task<TicketDTO> GetTicketById(Guid id, CancellationToken cancellationToken);
+    }
+}
