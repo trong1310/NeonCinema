479708641008b525f7c9b55ff738ea5e2ceef7e5
phongke004 <phongx543@gmail.com>
2024-08-01 19:43:16 +0700

    promotion, promotiontype

---
+++ NeonCinema_Application/DataTransferObject/Promotions/UpdatePromotionRequest.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NeonCinema_Application.DataTransferObject.Promotions
+{
+    public class UpdatePromotionRequest
+    {
+        public Guid PromotionID { get; set; }
+        public string PromotionCode { get; set; }
+        public string Description { get; set; }
+        public DateTime StartDate { get; set; }
+        public DateTime EndDate { get; set; }
+        public int Quantity { get; set; }
+        public string Proviso { get; set; }
+        public string Discount { get; set; }
+        public Guid PromotionTypeID { get; set; } 
+    }
+}
---
+++ NeonCinema_Infrastructure/Extention/AutoMapperProfile/PromotionProfile.cs
@@ -0,0 +1,28 @@
+﻿using AutoMapper;
+using NeonCinema_Application.DataTransferObject.PromotionCustomers;
+using NeonCinema_Application.DataTransferObject.Promotions;
+using NeonCinema_Application.DataTransferObject.PromotionTypes;
+using NeonCinema_Domain.Database.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NeonCinema_Infrastructure.Extention.AutoMapperProfile
+{
+    public class PromotionProfile : Profile
+    {
+        public PromotionProfile() {
+
+            CreateMap<Promotion, PromotionDTO>();
+            CreateMap<CreatePromotionRequest, Promotion>();
+            CreateMap<UpdatePromotionRequest, Promotion>();
+
+            CreateMap<PromotionType, PromotionTypesDTO>();
+
+            CreateMap<PromotionCustomer, PromotionCustomersDTO>();
+
+        }
+    }
+}
