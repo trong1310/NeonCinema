958cf33463e1ea8ce28c6b2f2a7c6971cf64a3ac
mrgiap736 <giaphipp@gmail.com>
2024-08-14 21:56:21 +0700

    SeatType

---
+++ NeonCinema_Application/Interface/Seats/ISeatTypeRepository.cs
@@ -0,0 +1,20 @@
+﻿using NeonCinema_Application.DataTransferObject.Seats;
+using NeonCinema_Application.DataTransferObject.SeatTypes;
+using NeonCinema_Domain.Database.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NeonCinema_Application.Interface.Seats
+{
+    public interface ISeatTypeRepository
+    {
+        Task<HttpResponseMessage> Create(SeatType seatT, CancellationToken cancellationToken);
+        Task<HttpResponseMessage> Update(SeatType seatT, CancellationToken cancellationToken);
+        Task<HttpResponseMessage> Delete(SeatType seatT, CancellationToken cancellationToken);
+        Task<List<SeatTypeDTO>> GetAll(CancellationToken cancellationToken);
+        Task<SeatTypeDTO> GetById(Guid id, CancellationToken cancellationToken);
+    }
+}
