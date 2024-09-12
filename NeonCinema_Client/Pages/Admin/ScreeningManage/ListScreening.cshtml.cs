7fba3afa8349e06c4163fd19bfd67d107507156d
mrgiap736 <giaphipp@gmail.com>
2024-09-12 17:23:17 +0700

    abc

---
+++ NeonCinema_Client/Pages/Admin/ScreeningManage/ListScreening.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace NeonCinema_Client.Pages.Admin.ScreeningManage
+{
+    public class ListScreeningModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
