7fd27432a345fcc7d94e4564662318a1ddf264c2
LEQUANGHAO29V1 <DELL@DESKTOP-8GC0563>
2024-09-24 08:28:58 +0700

    ..

--- NeonCinema_API/Controllers/Movie/MovieController.cs
+++ NeonCinema_API/Controllers/Movie/MovieController.cs
@@ -26,7 +26,7 @@ namespace NeonCinema_API.Controllers.Movie
             return Ok(obj);
         }
         [HttpPost ("Create")]
-        public async Task <IActionResult> Create( CreateMovieRequest request, CancellationToken cancellationToken)
+        public async Task <IActionResult> Create([FromForm] CreateMovieRequest request, CancellationToken cancellationToken)
         {
             var obj = await _reps.Create(_map.Map<NeonCinema_Domain.Database.Entities.Movies>(request),cancellationToken);
             return Ok(obj);
