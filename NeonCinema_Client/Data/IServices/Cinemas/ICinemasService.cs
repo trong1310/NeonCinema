c7d78cfc24ee9b9ccc3741d2bc80d40ce256ea8f
Cường Đặng Đức <gnouc2000@gmail.com>
2024-09-26 17:33:41 +0700

    LayoutAdmin

--- NeonCinema_Client/Data/IServices/Cinemas/ICinemasService.cs
+++ NeonCinema_Client/Data/IServices/Cinemas/ICinemasService.cs
@@ -4,7 +4,7 @@ namespace NeonCinema_Client.Data.IServices.Cinemas
 {
     public interface ICinemasService
     {
-        public Task<List<CinemasDTO>> GetAllCinemas(CancellationToken cancellationToken);
+        public Task<List<CinemasDTO>> GetAllCinemas();
         public Task<CinemasDTO> GetByIDRCinemas(Guid id, CancellationToken cancellationToken);
         public Task<HttpResponseMessage> CreateCinemas(CinemasCreateRequest request, CancellationToken cancellationToken);
         public Task<HttpResponseMessage> UpdateCinemas(Guid id, CinemasUpdateRequest request, CancellationToken cancellationToken);
