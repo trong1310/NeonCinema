fb3884852320fd59de7935740a2058c6a27b6826
LEQUANGHAO29V1 <DELL@DESKTOP-8GC0563>
2024-10-01 19:50:39 +0700

    fix mivie admin

--- NeonCinema_Client/Data/IServices/IMoviesServices/IMovieservices.cs
+++ NeonCinema_Client/Data/IServices/IMoviesServices/IMovieservices.cs
@@ -1,4 +1,8 @@
-﻿using NeonCinema_Application.DataTransferObject.Movie;
+﻿using NeonCinema_Application.DataTransferObject.Countrys;
+using NeonCinema_Application.DataTransferObject.Directors;
+using NeonCinema_Application.DataTransferObject.Genre;
+using NeonCinema_Application.DataTransferObject.Language;
+using NeonCinema_Application.DataTransferObject.Movie;
 using NeonCinema_Application.Pagination;
 
 namespace NeonCinema_Client.Data.IServices.IMoviesServices
@@ -6,5 +10,15 @@ namespace NeonCinema_Client.Data.IServices.IMoviesServices
     public interface IMovieservices
     {
         public Task<List<MovieDTO>> GetAllMovies();
+        public Task<List<GenreDTO>> GetAllGenre();
+        public Task<List<CountryDTO>> GetAllCountry();
+        public Task<List<DirectorDTO>> GetAllDirector();
+        public Task<List<LanguageDTO>> GetAllLanguage();
+        public Task<bool> CreateMovie(CreateMovieRequest input);
+        public Task<List<MovieDTO>> FilMovie(List<MovieDTO> lst,
+         Func<MovieDTO, bool> dk1 = null,
+         Func<MovieDTO, bool> dk2 = null,
+         Func<MovieDTO, bool> dk3 = null,
+         Func<MovieDTO, bool> dk4 = null);
     }
 }
