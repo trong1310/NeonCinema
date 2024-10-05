0c6df77b01882c283a39bbd6c1696e8e23ea4663
Vann Trongg <vantrongvt1310@gmail.com>
2024-10-05 17:23:41 +0700

    update db - add table promotion

--- NeonCinema_Domain/Database/Entities/PromotionUsers.cs
+++ NeonCinema_Domain/Database/Entities/PromotionUsers.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using NeonCinema_Domain.Database.Base;
+using NeonCinema_Domain.Enum;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,14 +8,15 @@ using System.Threading.Tasks;
 
 namespace NeonCinema_Domain.Database.Entities
 {
-    public class PromotionMovie
+    public class PromotionUsers : EntityBase
     {
         public Guid ID { get; set; }  // Khóa chính
         public Guid PromotionID { get; set; }  // Khóa ngoại đến Promotion
-        public Guid MovieID { get; set; }  // Khóa ngoại đến Movies
+        public Guid UserID { get; set; }  // Khóa ngoại đến user
+        public EntityStatus Status { get; set; }
 
         // Liên kết
         public virtual Promotion? Promotion { get; set; }
-        public virtual Movies? Movies { get; set; }
+        public virtual Users? User { get; set; }
     }
 }
