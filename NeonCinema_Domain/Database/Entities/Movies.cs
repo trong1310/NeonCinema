f72b622c651ae7c7f8f2cfdf431de520808cbd3f
phongke004 <phongx543@gmail.com>
2024-10-08 21:01:10 +0700

    commit sửa lỗi

--- NeonCinema_Domain/Database/Entities/Movies.cs
+++ NeonCinema_Domain/Database/Entities/Movies.cs
@@ -36,7 +36,7 @@ namespace NeonCinema_Domain.Database.Entities
         public virtual Director? Director { get; set; }
         public virtual ICollection<CategoryMovies>? CategoryMovies { get; set; }
         public virtual Language? Lenguage { get; set; }
-        public virtual Category? Genre { get; set; }
+        public virtual Genre? Genre { get; set; }
         public virtual Countrys? Countrys { get; set; }
        
         //
