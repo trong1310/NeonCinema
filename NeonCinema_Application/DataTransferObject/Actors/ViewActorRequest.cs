9d14e163f4075bfc57fb5ad9cd265c0f20026411
Vann Trongg <vantrongvt1310@gmail.com>
2024-11-03 12:28:58 +0700

    trong comiit db

---
+++ NeonCinema_Application/DataTransferObject/Actors/ViewActorRequest.cs
@@ -0,0 +1,15 @@
+﻿using NeonCinema_Application.Pagination;
+using NeonCinema_Domain.Enum;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NeonCinema_Application.DataTransferObject.Actors
+{
+    public class ViewActorRequest : PaginationRequest
+    {
+        public string? SearchName { get; set; }
+    }
+}
