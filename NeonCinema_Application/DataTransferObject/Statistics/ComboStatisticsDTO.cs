66446c69d69f206096501e378fcc68b938f176e6
phongke004 <phongx543@gmail.com>
2024-12-03 08:06:32 +0700

    commit thống kê

---
+++ NeonCinema_Application/DataTransferObject/Statistics/ComboStatisticsDTO.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NeonCinema_Application.DataTransferObject.Statistics
+{
+	public class ComboStatisticsDTO
+	{
+
+		public Guid ComboID { get; set; }
+		public string ComboName { get; set; }
+		public int QuantitySold { get; set; }
+	}
+}
