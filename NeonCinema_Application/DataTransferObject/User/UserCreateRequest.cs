ba9481d303bb5c29721a1a96d149e319b9bc19e9
Cường Đặng Đức <gnouc2000@gmail.com>
2025-01-05 17:38:20 +0700

    fe

--- NeonCinema_Application/DataTransferObject/User/UserCreateRequest.cs
+++ NeonCinema_Application/DataTransferObject/User/UserCreateRequest.cs
@@ -14,18 +14,9 @@ namespace NeonCinema_Application.DataTransferObject.User
 	public class UserCreateRequest
 	{
 		public Guid Id { get; set; }
-		//[Required(ErrorMessage = "Họ và tên không được để trống")]
-  //      [RegularExpression(@"^[\p{L}\s]+$", ErrorMessage = "Họ và tên không được chứa số hoặc ký tự đặc biệt.")]
         public string FullName { get; set; }
-
-		//[Required(ErrorMessage = "Số điện thoại không được để trống")]
-		//[RegularExpression(@"^(03|08|09)\d{8}$", ErrorMessage = "Số điện thoại phải bắt đầu bằng 03, 08 hoặc 09 và có đúng 10 chữ số.")]
 		public string PhoneNumber { get; set; }
-
-		//[Required(ErrorMessage = "Email không được để trống")]
-		//[EmailAddress(ErrorMessage = "Email không đúng định dạng")]
 		public string Email { get; set; }
-
 		public DateTime DateOrBriht { get; set; }
 		public string? Images { get; set; }
 		public bool? Gender { get; set; }
